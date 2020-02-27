0a145007e6a6c58452c30b1776a39c1a62f11a20
unknown <alfabetatango9@gmail.com>
2020-02-27 14:48:36 +0100

    init

---
+++ mypm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -e "\e[35m                        ____                                                                     \n /'\_/ \               /\  _ \   /'\_/ \                                                         \n/\      \    __  __    \ \ \L\ \/\      \                                                        \n\ \ \__\ \  /\ \/\ \    \ \ ,__/\ \ \__\ \                                                       \n \ \ \_/\ \ \ \ \_\ \    \ \ \/  \ \ \_/\ \                                                      \n  \ \_\\ \_\ \/ ____ \    \ \_\   \ \_\\ \_\                                                     \n   \/_/ \/_/   /___/> \    \/_/    \/_/ \/_/                                                     \n                 /\___/                                                                          \n\e[0m"
+
+echo
+echo
+
+
+
+echo -e "\e[33m=== MyPm is intalled, now you can run \e[36mbash mypm.sh\e[33m anywhere ===\e[0m"
+echo
+echo -e "\e[33m=== \e[36mEnjoy Coding !\e[33m ===\e[0m"
